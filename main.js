acd086f062cb7704a30a6d61ef0bd4e3aa739f58
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-27 09:30:24 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const lista = document.getElementById('lista')
+
+fetch('https://jsonplaceholder.typicode.com/posts').then((resp) => resp.json()).then((data) => {
+
+data.forEach((post) => {
+    let titulo = post.title;
+    let corpo = post.body;
+    const li = document.createElement('li')
+
+    li.innerHTML = "<h4>" + titulo + "<h4>" + "<p>" + corpo + "<p>";
+    lista.append(li)
+});
+
+})
